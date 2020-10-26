6236f2328f5e9d71a693fd30e5cec6a6f74dfe5c
Isla <fallenacademy@gmail.com>
2020-10-26 13:05:21 -0700

    Set up random generator

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 let endpoint = "https://www.jsonstore.io/8ba4fd855086288421f770482e372ccb5a05d906269a34da5884f39eed0418a1"
+
+function getRandom(){
+    let random_str = Math.random().toString(5).substring(2,5) + Math.random().toString(10).substring(2,5);
+    return random_str()
+}
